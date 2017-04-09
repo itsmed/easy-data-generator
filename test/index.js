2560800dd4a68cc4e7244cdf9393391c7a148abf
DejonGill <dejonjgill@gmail.com>
2017-04-09 14:54:08 -0700

    initial test

--- test/index.js
+++ test/index.js
@@ -4,8 +4,10 @@ const sinon = require('sinon');
 const dg = require('../index');
 
 describe('Easy Data Generator', () => {
-  dg('helloWorld.ruby')
   it('Should do something', () => {
-    expect(3 * 3).to.equal(9);
+    let spy = sinon.spy(dg);
+    dg('fake.txt');
+    sinon.assert.called(spy);
+    sinon.asset.calledWith(spy, 'fake.txt');
   });
 });
