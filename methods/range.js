e3d9d110270fa25857b00b3dae8baf891fc34ece
DejonGill <dejonjgill@gmail.com>
2017-04-10 12:46:46 -0700

    init

--- methods/range.js
+++ methods/range.js
@@ -1,15 +1,19 @@
 const randInt = require('./randInt');
 
-module.exports = function range(len = 10) {
+const { errorMessage } = require('../helpers');
 
+module.exports = function range(len = 10, stop = 10) {
+  if (len < 0) {
+    return errorMessage('Range method requires a positive number for first argument.\nRange Signature:\t\tfunction range(length, start, stop)\nAll arguments are optional.', 'A positive integer or 0', len);
+  }
   let arr = [];
   let i = 0;
   let val = 0;
-  while (i <= len) {
+  while (i < len) {
     arr.push(val);
     val++;
     i++;
-    if (val > 10) {
+    if (val > stop) {
       val = 0;
     }
   }
